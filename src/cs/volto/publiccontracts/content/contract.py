f132475ee6b06f2efcb24b71363e8ad9f4b57aca
Lur Ibargutxi <libargutxi@codesyntax.com>
2022-05-18 15:39:28 +0200

    implementer

--- src/cs/volto/publiccontracts/content/contract.py
+++ src/cs/volto/publiccontracts/content/contract.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from zope.interface import implementer
 
 
 class IContract(model.Schema):
-    """ Marker interface for Contract
-    """
+    """Marker interface for Contract"""
+
+
+@implementer(IContract)
+class Contract(Container):
+    pass
--- src/cs/volto/publiccontracts/content/contracts_folder.py
+++ src/cs/volto/publiccontracts/content/contracts_folder.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from zope.interface import implementer
 
 
 class IContractsFolder(model.Schema):
-    """ Marker interface for ContractsFolder
-    """
+    """Marker interface for ContractsFolder"""
+
+
+@implementer(IContractsFolder)
+class ContractsFolder(Container):
+    pass
