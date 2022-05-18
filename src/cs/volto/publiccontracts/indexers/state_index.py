37e0ba63369e936277d506f49d92f3603af93b0c
Lur Ibargutxi <libargutxi@codesyntax.com>
2022-05-18 16:00:32 +0200

    index

--- src/cs/volto/publiccontracts/indexers/state_index.py
+++ src/cs/volto/publiccontracts/indexers/state_index.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from plone.app.contenttypes.interfaces import IDocument
+from cs.volto.publiccontracts.content.contract import IContract
 from plone.dexterity.interfaces import IDexterityContent
 from plone.indexer import indexer
 
 
 @indexer(IDexterityContent)
 def dummy(obj):
-    """ Dummy to prevent indexing other objects thru acquisition """
-    raise AttributeError('This field should not indexed here!')
+    """Dummy to prevent indexing other objects thru acquisition"""
+    raise AttributeError("This field should not indexed here!")
 
 
-@indexer(IDocument)  # ADJUST THIS!
+@indexer(IContract)
 def state_index(obj):
     """Calculate and return the value for the indexer"""
-    return obj.state_index
+    return obj.state
